d7313dabded9231f981ab7941e66b93164d6c81b
Busanz <buddhisanz@gmail.com>
2026-02-22 21:13:53 +0530

    Fix mobile display

--- public/scripts/activeLink.js
+++
@@ -1,9 +0,0 @@
-const navLinks = document.querySelectorAll('.header_link');
-
-navLinks.forEach((link) => {
-  if (link.getAttribute('href') === window.location.pathname) {
-    link.classList.add('active');
-  } else {
-    link.classList.remove('active');
-  }
-});
